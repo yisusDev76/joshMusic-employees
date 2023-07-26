4d4106743d2417c75f1d39ae404dcce6061f6dd0
yisusDev76 <je.parra.navarrete@gmail.com>
2023-07-26 17:43:58 -0600

    files to start the template

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,4 +10,12 @@ export const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full',
   },
+  {
+    path: 'intro',
+    loadComponent: () => import('./pages/intro/intro.page').then( m => m.IntroPage)
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
+  },
 ];
